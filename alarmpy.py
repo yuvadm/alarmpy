481c77e57edb7e82b87c88fe76c2b723ff80b445
Yuval Adam <_@yuv.al>
2021-05-13 01:40:31 +0300

    Initial version

---
+++ alarmpy.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+import requests
+
+HEADERS = {
+    'Referer': 'https://www.oref.org.il/11226-he/pakar.aspx',
+    'X-Requested-With': 'XMLHttpRequest',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+}
+
+URL = 'https://www.oref.org.il/WarningMessages/alert/alerts.json'
+
+def fetch():
+    res = requests.get(URL, headers=HEADERS)
+    if res.content:
+        print(datetime.now().isoformat(), res.json())
+
+
+if __name__ == "__main__":
+    fetch()
