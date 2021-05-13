b44014d415764dfbd1cec9c5951c63bdb9af7e7c
Yuval Adam <_@yuv.al>
2021-05-13 14:50:18 +0300

    Black formatting

--- alarmpy.py
+++ alarmpy.py
@@ -4,12 +4,13 @@ from time import sleep
 import requests
 
 HEADERS = {
-    'Referer': 'https://www.oref.org.il/11226-he/pakar.aspx',
-    'X-Requested-With': 'XMLHttpRequest',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+    "Referer": "https://www.oref.org.il/11226-he/pakar.aspx",
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
 }
 
-URL = 'https://www.oref.org.il/WarningMessages/alert/alerts.json'
+URL = "https://www.oref.org.il/WarningMessages/alert/alerts.json"
+
 
 def fetch():
     res = requests.get(URL, headers=HEADERS)
