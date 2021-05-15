4121e69cda7e57314566269af67a98e1ac3f7755
Yuval Adam <_@yuv.al>
2021-05-15 17:03:39 +0300

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="alarmpy",
+    version="1.0.0",
+    author="Yuval Adam",
+    author_email="_@yuv.al",
+    description="Pikud Ha'oref Alarm Tracking",
+    license="GPLv3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yuvadm/alarmpy",
+    project_urls={
+        "Bug Tracker": "https://github.com/yuvadm/alarmpy/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+    ],
+    package_dir={"": "alarmpy"},
+    packages=setuptools.find_packages(where="alarmpy"),
+    python_requires=">=3.6",
+    install_requires=["requests", "click"],
+)
