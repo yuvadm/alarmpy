00d9d2c585738fe00ee8dda1ce620869d9fc5a01
Yuval Adam <_@yuv.al>
2022-04-21 08:43:07 +0300

    Release v1.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="alarmpy",
-    version="1.4.2",
+    version="1.5.0",
     author="Yuval Adam",
     author_email="_@yuv.al",
     description="Pikud Ha'oref Alarm Tracking",
